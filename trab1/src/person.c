ffcb382464a918f70b6bab3ea83ec037359d276a
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2022-08-28 15:14:06 -0300

    fix string copying in createPerson func

--- trab1/src/person.c
+++ trab1/src/person.c
@@ -8,11 +8,7 @@ Person *createPerson(char *rg, char *name)
     Person *person = malloc(sizeof(Person));
 
     sprintf(person->rg, "%s", rg);
-    //strcpy(person->rg, rg); 
-    person->rg[8] = '\0';
     sprintf(person->name, "%s", name);
-    //strcpy(person->name, name);
-    person->name[30] = '\0';
 
     return person;
 }
