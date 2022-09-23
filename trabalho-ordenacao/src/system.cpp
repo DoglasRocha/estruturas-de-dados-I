f29a6e243ed42c3490b7f9b86e466eaa789c2463
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2022-09-23 00:05:20 -0300

    implement binary search

--- trabalho-ordenacao/src/system.cpp
+++ trabalho-ordenacao/src/system.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include "../includes/system.hpp"
 #include "../common/includes/person.hpp"
 #include "../sequential-list/includes/sequential-list.hpp"
@@ -287,44 +288,44 @@ void System::searchingMenu(long rg, int *C, int *M)
 
     if (option != 1 && option != 2)
         searchingMenu(rg, C, M);
-    (*C)++;
 
     if (option == 1)
         sequentialSearch(rg, C, M);
 
     else
         binarySearch(rg, C, M);
-
-    (*C)++;
 }
 
 void System::binarySearch(long rg, int *C, int *M)
 {
-    int currentPos = list->getLength();
+    int currentPos = (int) round((list->getLength() - 1) / 2.0),
+        increase = currentPos;
+    (*M) += 2;
     start = clock();
 
     do
     {
-        (*C) += 2;
+        increase = increase == (int) round(increase / 2.0) ? 0 : (int) round(increase / 2.0);
+        (*C)++, (*M)++;
 
         if ((*list)[currentPos]->rg > rg)
         {
-            currentPos -= (currentPos / 2);
+            currentPos += -increase;
+            (*M)++;
             (*C)++;
-            cout << (*list)[currentPos]->rg << "," << currentPos << ",-" << endl;
             continue;
         }
         else if ((*list)[currentPos]->rg < rg)
         {
-            currentPos += (currentPos / 2);
+            currentPos += increase;
+            (*M)++;
             (*C) += 2;
-            cout << (*list)[currentPos]->rg << "," << currentPos << ",+" << endl;
             continue;
         }
 
         (*C) += 2;
     }
-    while ((*list)[currentPos]->rg != rg);
+    while ((*list)[currentPos]->rg != rg && increase > 0);
 
     if ((*list)[currentPos]->rg == rg)
         printData((*list)[currentPos], currentPos, C, M);
