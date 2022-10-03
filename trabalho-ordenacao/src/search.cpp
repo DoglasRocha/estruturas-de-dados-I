e3ce06f8b6c662bacd23bc2ce463e39146b6bfab
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2022-10-02 21:59:25 -0300

    spilt system implementation in separate files

---
+++ trabalho-ordenacao/src/search.cpp
@@ -0,0 +1,55 @@
+#include "../includes/system.hpp"
+
+void System::searchingMenu(long rg, int *C, int *M)
+{
+    cout << "Qual busca você deseja utilizar?" << endl
+         << "(1) Sequencial" << endl
+         << "(2) Binária" << endl;
+    cin >> option;
+
+    if (option != 1 && option != 2)
+        searchingMenu(rg, C, M);
+
+    if (option == 1)
+        sequentialSearch(rg, C, M);
+
+    else
+        binarySearch(rg, C, M);
+}
+
+void System::binarySearch(long rg, int *C, int *M)
+{
+    int currentPos = (int) round((list->getLength() - 1) / 2.0),
+        increase = currentPos;
+    (*M) += 2;
+    start = clock();
+
+    do
+    {
+        increase = increase == (int) round(increase / 2.0) ? 0 : (int) round(increase / 2.0);
+        (*C)++, (*M)++;
+
+        if ((*list)[currentPos]->rg > rg)
+        {
+            currentPos += -increase;
+            (*M)++;
+            (*C)++;
+            continue;
+        }
+        else if ((*list)[currentPos]->rg < rg)
+        {
+            currentPos += increase;
+            (*M)++;
+            (*C) += 2;
+            continue;
+        }
+
+        (*C) += 2;
+    }
+    while ((*list)[currentPos]->rg != rg && increase > 0);
+
+    if ((*list)[currentPos]->rg == rg)
+        printData((*list)[currentPos], currentPos, C, M);
+    else
+        printData(nullptr, 0, C, M);
+}
